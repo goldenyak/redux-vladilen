9b783c39c23dc2b5cc691cc8bf6c584b132e3587
goldenyak <goldenyakegor@gmail.com>
2021-12-07 22:59:47 +0300

    add some functions

--- my-app/src/components/PostForm.js
+++ my-app/src/components/PostForm.js
@@ -15,9 +15,10 @@ export default class PostForm extends React.Component {
         return (
             <form onSubmit={this.submitHandler}>
                <div className='form-group'>
-                   <label htmlFor='exampleInputPassword1'>Заголовок поста</label>
-                   <input type='text' className='form-control' id='exampleInputPassword1'/>
+                   <label htmlFor='title'>Заголовок поста</label>
+                   <input type='text' className='form-control' id='title'/>
                </div>
+                <button className='btn btn-success mt-3 mb-3' type='submit'>Создать</button>
             </form>
         )
     }
