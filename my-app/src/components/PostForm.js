cacd105f9c6f0ca7cbaa5928d4bb92330abfb3e8
goldenyak <goldenyakegor@gmail.com>
2021-12-07 23:08:05 +0300

    add some functions

--- my-app/src/components/PostForm.js
+++ my-app/src/components/PostForm.js
@@ -4,20 +4,32 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            title: '',
+        }
     }
 
     submitHandler = (event) => {
         event.preventDefault();
     }
 
+    changeInputHandler = (event) => {
+        this.setState(prev => ({...prev, ...{[event.target.name]: [event.target.value]}})
+        )
+    }
+
     render() {
         return (
             <form onSubmit={this.submitHandler}>
-               <div className='form-group'>
-                   <label htmlFor='title'>Заголовок поста</label>
-                   <input type='text' className='form-control' id='title'/>
-               </div>
+                <div className='form-group'>
+                    <label htmlFor='title'>Заголовок поста</label>
+                    <input type='text'
+                           className='form-control'
+                           id='title'
+                           value={this.state.title}
+                           name='title'
+                           onChange={this.changeInputHandler}/>
+                </div>
                 <button className='btn btn-success mt-3 mb-3' type='submit'>Создать</button>
             </form>
         )
