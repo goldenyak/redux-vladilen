a7a92700d5aef3fb016cd146fdcc23ab64d40115
goldenyak <goldenyakegor@gmail.com>
2021-12-07 22:09:59 +0300

    add some functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,24 @@
+import {Button} from "bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+
 function App() {
+
     return (
-        <div className="App">
-            Hello
+        <div className="container pt-3">
+            <div className="row">
+                <div className="col">
+                    form
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    posts
+                </div>
+                <div className="col">
+                    post
+                </div>
+            </div>
         </div>
     );
 }
