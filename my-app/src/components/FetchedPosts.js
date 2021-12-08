374cbdaa646b71df0a54df26d07d78b511ae4f56
goldenyak <goldenyakegor@gmail.com>
2021-12-08 22:27:46 +0300

    add some functions

--- my-app/src/components/FetchedPosts.js
+++ my-app/src/components/FetchedPosts.js
@@ -2,10 +2,20 @@ import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPosts} from "../redux/actions";
 import Post from "./Post";
+import {postsReducer} from "../redux/postsReduser";
 
 export default () => {
     const dispatch = useDispatch()
     const posts = useSelector(state => state.posts.fetchedPosts)
+    const loading = useSelector(state => state.app.loading)
+
+    if (loading === true) {
+        return (
+            <div className="spinner-border text-danger" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        )
+    }
 
     if (!posts.length) {
         return <button
